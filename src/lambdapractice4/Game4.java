8e24a4be33fdb1e095587dbb48d38db9b5401db2
sksjw95 <tjwldnd95@hanmail.net>
2023-07-21 18:56:21 +0900

    매개변수:O,반환값:O

---
+++ src/lambdapractice4/Game4.java
@@ -0,0 +1,5 @@
+package lambdapractice4;
+
+public interface Game4 {
+    int play(int a, int b);
+}
---
+++ src/lambdapractice4/Main8.java
@@ -0,0 +1,9 @@
+package lambdapractice4;
+
+public class Main8 {
+    public static void main(String[] args) {
+        Game4 game4 = (a,b)->{return a*b ;};
+        System.out.println(game4.play(7,6));
+
+    }
+}
